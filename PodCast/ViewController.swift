450fa503af4e615f1f1c1ff4ad2241a2b38ad138
Keith Cao <caoyonghao@hotmail.co.nz>
2018-09-29 14:42:07 +1200

    Initial Commit

---
+++ PodCast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodCast
+//
+//  Created by Keith Cao on 29/09/18.
+//  Copyright © 2018 Keith Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
